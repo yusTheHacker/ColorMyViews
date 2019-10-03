4a1cc8e477ffcb5f37dc70e5e453dd1d45602b01
Yusley <5003489915@student.csn.edu>
2019-10-03 02:17:37 -0700

    Changed code to make the three button to change the boxes colors.

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +19,8 @@ class MainActivity : AppCompatActivity() {
     private fun setListener() {
 
         var clickableViews: List<View> = listOf(box_one_text, box_two_text, box_three_text,
-                                                    box_four_text, box_five_text, constriant_layout)
+                                                    box_four_text, box_five_text, constriant_layout,
+                                                    red_button, green_button, yellow_button)
 
         for (item in clickableViews)
             item.setOnClickListener{makeColored(it)}
@@ -36,6 +38,13 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+
+            //Boxes using custom color for background
+
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
